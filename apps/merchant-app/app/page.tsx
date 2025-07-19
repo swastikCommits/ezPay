55048ed456b393c136666a63c6985be050b7a4f2
swastikCommits <textswastik.alt@gmail.com>
2025-07-19 22:28:14 +0530

    init

---
+++ apps/merchant-app/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return(
+    <div className="flex flex-col items-center justify-center h-screen bg-amber-300">
+      Hello Merchant App
+    </div>
+  )
+}
---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,8 @@
+"'use client'"
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-blue-300">
+      Hello User App
+    </div>
+  )
+}
