ed811a71e2e86e927569e983110bb8f1ee44d04a
swastikCommits <textswastik.alt@gmail.com>
2025-07-22 05:47:34 +0530

    added pages and its components

---
+++ packages/ui/src/code.tsx
@@ -0,0 +1,9 @@
+export function Code({
+    children,
+    className,
+  }: {
+    children: React.ReactNode;
+    className?: string;
+  }){
+    return <code className={className}>{children}</code>;
+  }
