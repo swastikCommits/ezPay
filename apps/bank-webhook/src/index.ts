6ce9ccab60509f72179acde306d394cb676d1431
swastikCommits <textswastik.alt@gmail.com>
2025-07-25 16:43:24 +0530

    added todo for bank-webhook

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -5,8 +5,9 @@ const app = express();
 app.use(express.json())
 
 app.post("/hdfcWebhook", async (req, res) => {
-    //TODO: Add zod validation here?
+    //TODO: Add zod validation here
     //TODO: HDFC bank should ideally send us a secret so we know this is sent by them
+    //TODO: Check if the OnRamp TXn is processing or not
     const paymentInformation: {
         token: string;
         userId: string;
